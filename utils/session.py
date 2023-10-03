6a946a3fb45a788d1ea0e3e47a7dbf783e520f09
tuandq <tuandq@ssi.com.vn>
2023-10-03 16:13:33 +0700

    dirty version of authen, next rorders, push orders

---
+++ utils/session.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import requests
+
+
+def fetch_session():
+    st.session_state['authenticated'] = False
+    response = requests.post(
+        'https://localhost:5000/v1/api/iserver/auth/status', timeout=60, verify=False)
+    if response.status_code != 200:
+        return
+    response_body = response.json()
+    if not response_body.get('authenticated'):
+        return
+    st.session_state['authenticated'] = True
+    if 'selected_account' not in st.session_state:
+        response = requests.get(
+            'https://localhost:5000/v1/api/iserver/accounts', timeout=60, verify=False)
+        response_body = response.json()
+        st.session_state['selected_account'] = response_body['selectedAccount']
